eacc006994d640057819cdc6aa3f80a1b8b57543
berkaybilik <bilik.berkay@gmail.com>
2025-12-15 23:16:46 +0000

    IMAGE

--- file_for_images.py
+++ file_for_images.py
@@ -9,9 +9,7 @@ def handle_charge(payment_id: str, status: PaymentStatus) -> None:
 def process_webhook(payload: dict) -> None:
     ...
     event = payload.get("event")
-    #FREO: Stripe migrated to sending `charge.completed` from `charge.succeeded` 6 months ago
-    if event == "charge.succeeded":
-        return handle_charge(payment_id=payload["id"], status=PaymentStatus.COMPLETED)
     
+    #FREO: Stripe migrated to sending `charge.completed` from `charge.succeeded` 6 months ago
     if event == "charge.completed":
         return handle_charge(payment_id=payload["id"], status=PaymentStatus.COMPLETED)
