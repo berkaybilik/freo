79671a9158a74a8836909136987592a1e3c2d869
berkaybilik <bilik.berkay@gmail.com>
2025-12-15 23:28:30 +0000

    x

--- file_for_images.py
+++ file_for_images.py
@@ -10,8 +10,6 @@ def process_webhook(payload: dict) -> None:
     ...
     event = payload.get("event")
 
-    if event == "charge.succeeded":
-        return handle_charge(payment_id=payload["id"], status=PaymentStatus.COMPLETED)
-
+    # FREO: Stripe migrated to sending `charge.completed` from `charge.succeeded` 6 months ago
     if event == "charge.completed":
         return handle_charge(payment_id=payload["id"], status=PaymentStatus.COMPLETED)
